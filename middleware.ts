5803cc026b94329b285ce38c0d6e4ac662a026e9
unknown <abdulraufyuinis@gmail.com>
2025-09-19 23:30:06 +0300

    Changes to be committed: 	new file:   app/(signed-in)/dashboard/page.tsx 	new file:   app/(signed-in)/layout.tsx 	new file:   convex/schema.ts 	modified:   middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isAuhtenticatedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isAuhtenticatedRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
