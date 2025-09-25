286c7a713e72bdb5db4ce3f26dae42a85566d26d
unknown <abdulraufyuinis@gmail.com>
2025-09-25 15:29:04 +0300

    modified:   app/(signed-in)/dashboard/page.tsx 	modified:   components/app-sidebar.tsx

--- app/(signed-in)/dashboard/page.tsx
+++ app/(signed-in)/dashboard/page.tsx
@@ -1,7 +1,81 @@
-import React from 'react'
+'use client'
+
+import { Channel, ChannelHeader, divMod, useChatContext } from "stream-chat-react"
+ 11;
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useSidebar } from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
+import { LogOutIcon, VideoIcon } from "lucide-react";
+import { Window } from "stream-chat-react";
+import { MessageList } from "stream-chat-react";
+import { MessageInput } from "stream-chat-react";
+import { Thread } from "stream-chat-react";
 
 function Dashboard() {
-  return <div>Dashboard</div>
+
+  const { user } = useUser();
+  const router = useRouter();
+  const { channel, setActiveChannel } = useChatContext();
+  const { setOpen } = useSidebar();
+
+  const handleCall = () => {
+    console.log("Calling...");
+  };
+
+  const handleLeaveChat = () => {
+    console.log("Leaving chat...");
+  };
+
+  return (
+    <div className="flex flex-col w-full f;ex-1">
+      {channel ? (
+        <Channel>
+          <Window>
+            <div className="flex items-center justify-between">
+              {channel.data?.member_count === 1 ? (
+                <ChannelHeader title="You are the only one in this chat!"/>
+              ) : (
+                <ChannelHeader />
+              )}
+              <div className="flex items-center gap-2">
+                <Button variant="outline" onClick={handleCall}>
+                  <VideoIcon className="h-4 w-4" />
+                  Video Call
+                </Button>
+
+                <Button 
+                variant="outline" 
+                onClick={handleLeaveChat}
+                className="text-red-500 hover:text-red-600 hover:bg-red-50
+                dark:hover:bg-red-950"
+                >
+                   <LogOutIcon className="h-4 w-4" />
+                    Leave Chat
+                </Button>
+              </div>
+              <MessageList/>
+
+              <div className="sticky bottom-0 w-full">
+                <MessageInput />  
+              </div>
+            </div>
+          </Window>
+          <Thread/>
+        </Channel>
+      ) : (
+        <div className="flex flex-col items-center justify-center h-full">
+          <h2 className="text-2xl font-semibold text-muted-foreground mb-4">
+            No chat selected
+          </h2>
+          <p className="text-muted-foreground">
+            Select a chat from the sidebar or start new conversation
+            </p>
+        </div>
+      )}
+
+    </div>
+  );
 }
 
 export default Dashboard;
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -16,11 +16,26 @@ import {
 } from "@/components/ui/sidebar"
 import { Button } from "./ui/button"
 import { UserButton, useUser } from "@clerk/nextjs"
+import { ChannelList } from "stream-chat-react"
+import { Channel } from "diagnostics_channel"
+import { ChannelFilters, ChannelSort } from "stream-chat"
+
 
 
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const { user } = useUser();
+
+const filters: ChannelFilters = {
+  members: { $in: [user?.id as string] },
+  type: {$in: ["messaging", "team"]}, //Show both messaging and team channels
+};
+
+const options = { state: true, presence: true };
+const sort: ChannelSort = { 
+  last_message_at: -1
+ };
+
   return (
     <Sidebar variant="floating" {...props}>
       <SidebarHeader>
@@ -49,6 +64,26 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               Start New Chat
             </Button>
             {/*  Channels List */}
+            <ChannelList 
+            filters={filters} 
+            options={options} 
+            sort={sort} 
+            EmptyStateIndicator={() => ( 
+              <div className="flex flex-col items-center justify-center 
+              h-full py-12 px-4">
+                <div className="text-6xl mb-6 opacity-20">💬
+</div>
+                <h2 className="text-xl font-medium text-foreground mb-2">
+                  Ready to chat?
+                </h2>
+                <p className="text-sm text-muted-foreground text-center 
+                leading-relaxed max-w-[200px]">
+                  Your converstions appear once you start chatting with others.
+                </p>
+              </div>
+            )}
+             />
+
           </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
